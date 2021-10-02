39d5c2efbc536c57d52775e51f006c7f9aa25a3f
Xia <zeyaoliu@me.com>
2021-10-02 23:51:49 +0100

    Added add_entry method

--- week-4/encapsulation/secret-diary/lib/diary.rb
+++ week-4/encapsulation/secret-diary/lib/diary.rb
@@ -1,3 +1,14 @@
 class Diary
-  
+
+  attr_reader :entries
+
+  def initialize
+    # @title = title
+    # @text = text
+    @entries = []
+  end
+
+  def add_entry(entry)
+    @entries.push(entry)
+  end
 end
--- week-4/encapsulation/secret-diary/spec/diary_spec.rb
+++ week-4/encapsulation/secret-diary/spec/diary_spec.rb
@@ -2,6 +2,12 @@ require_relative "../lib/diary.rb"
 
 RSpec.describe Diary do
   describe "#add_entry" do
-    
+    let(:entry_time) { Time.now }
+    subject(:diary) { described_class.new }
+    let(:entry) { entry = double(:entry, entry_time: entry_time, text: "Dear Diary.") }
+
+    it 'adds an entry with a date, title and some text' do
+      expect(diary.add_entry(entry)).to include entry
+    end
   end
 end
